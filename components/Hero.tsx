d6f9aea047a0903ecfb0e4d1e49f05d42d0638ac
Kushal Gupta <103812291+kushalgupta1203@users.noreply.github.com>
2024-07-30 20:39:19 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,6 +4,7 @@ import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import { ImagesSlider } from "./ui/ImagesSlider";
 import { socialMedia } from "@/data";
+import Image from "next/image"; // Import next/image for optimized image loading
 
 const Hero = () => {
   return (
@@ -31,7 +32,7 @@ const Hero = () => {
       </div>
 
       <div className="flex flex-col items-center justify-between relative my-20 z-10 sm:flex-row sm:items-center">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center sm:items-start justify-center mt-[-100px]"> {/* Move upwards by 100px */}
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center sm:items-start justify-center mt-[-100px]">
           <TextGenerateEffect
             words="Hello,"
             className="uppercase tracking-widest text-l text-center sm:text-left text-blue-100 max-w-80"
@@ -49,20 +50,22 @@ const Hero = () => {
         </div>
         
         {/* ImagesSlider with kushal.png */}
-        <div className="relative w-full max-w-xs lg:max-w-sm xl:max-w-md h-[450px] mt-4 sm:mt-[-35px] sm:ml-[-15px]"> {/* Adjust positioning and size for small devices */}
+        <div className="relative w-full max-w-xs lg:max-w-sm xl:max-w-md h-[450px] mt-4 sm:mt-[-35px] sm:ml-[-15px]">
           <ImagesSlider
             images={['/kushal.png']}
             autoplay={true}
             direction="up"
             overlay={false}
             className="w-full h-full object-cover"
-            children={null}
-          />
+          >
+            {/* Providing an empty React node to satisfy the 'children' prop requirement */}
+            <></>
+          </ImagesSlider>
         </div>
       </div>
 
       {/* Magic Button on the top right */}
-      <div className="absolute top-[35px] right-4 z-20 sm:top-[25px] md:top-[8px] lg:top-[8px]"> {/* Move up by 10px on small screens */}
+      <div className="absolute top-[35px] right-4 z-20 sm:top-[25px] md:top-[8px] lg:top-[8px]">
         <a href="https://drive.google.com/file/d/1lAyiSmqHePmibibz3SmUQQSyOlqBHbMv/view?usp=sharing" target="_blank" rel="noopener noreferrer">
           <MagicButton
             title="Resume"
@@ -82,7 +85,7 @@ const Hero = () => {
             rel="noopener noreferrer"
             className="w-6 h-6 sm:w-12 sm:h-12 md:w-12 md:h-12 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border-black-300 sm:border-2 border-0"
           >
-            <img src={info.img} alt="icons" width={24} height={24} />
+            <Image src={info.img} alt="icons" width={24} height={24} />
           </a>
         ))}
       </div>
