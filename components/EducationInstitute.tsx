df56b678aa15dcf9517ff702c80268556beb3aff
Kushal Gupta <103812291+kushalgupta1203@users.noreply.github.com>
2024-07-31 07:53:24 +0530

    Update EducationInstitute.tsx

--- components/EducationInstitute.tsx
+++ components/EducationInstitute.tsx
@@ -7,20 +7,14 @@ import { InfiniteMovingCards } from "./ui/InfiniteCards";
 const EducationInstitute = () => {
   return (
     <section id="education" className="py-20">
-      <h1 className="heading">
+      <h1 className="heading mb-4 sm:mb-10">
         Academic achievements from
         <span className="text-purple"> my educational journey</span>
       </h1>
 
-      <div className="flex flex-col items-center max-lg:mt-10">
-        <div
-          className="h-[130vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden"
-        >
-          <InfiniteMovingCards
-            items={education}
-            direction="right"
-            speed="fast"
-          />
+      <div className="flex flex-col items-center">
+        <div className="h-[130vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden mt-4 sm:mt-10">
+          <InfiniteMovingCards items={education} direction="right" speed="fast" />
         </div>
       </div>
     </section>
