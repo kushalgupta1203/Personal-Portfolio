5a48cd8e09c962dbd913f21f4915b7d57ee59863
Kushal Gupta <kushalgupta1203@gmail.com>
2025-03-21 16:36:12 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -66,7 +66,7 @@ const Hero = () => {
 
       {/* Magic Button on the top right */}
       <div className="absolute top-[35px] right-4 z-20 sm:top-[25px] md:top-[8px] lg:top-[8px]">
-        <a href="https://drive.google.com/file/d/1gNwuhHcfTILz9Mvg2pXGmSMJDMgX2DH2/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+        <a href="https://drive.google.com/file/d/1gNwuhHcfTILz9Mvg2pXGmSMJDMgX2DH2/view?usp=sharing" target="_blank" rel="noopener noreferrer">
           <MagicButton
             title="Resume"
             icon={<FaLocationArrow />}
