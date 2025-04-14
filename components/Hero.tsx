96b92ae1b4982a28cc8ef9144f6496ad94af6424
Kushal Gupta <kushalgupta1203@gmail.com>
2025-04-14 17:11:20 +0530

    updated photo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -52,7 +52,7 @@ const Hero = () => {
         {/* ImagesSlider with kushal.png */}
         <div className="relative w-full max-w-xs lg:max-w-sm xl:max-w-md h-[450px] mt-4 sm:mt-[-35px] sm:ml-[-15px]">
           <ImagesSlider
-            images={['/professional_pic_circle.png']}
+            images={['/professional_pic_circle_crop_low_resolution.png']}
             autoplay={true}
             direction="up"
             overlay={false}
