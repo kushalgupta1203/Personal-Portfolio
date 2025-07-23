e4c9e16b0f21cde6297abc45d5f6e86bffbdfea1
Kushal Gupta <kushalgupta1203@gmail.com>
2025-07-23 15:42:17 +0530

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -44,7 +44,7 @@ const Hero = () => {
           />
 
           <TextGenerateEffect
-            words="Software Developer | AI & Data Science Enthusiast | Ex-Intern @ MNRE"
+            words="Software Developer, AI Enthusiast & Former SDE Intern @ MNRE"
             className="text-center sm:text-left text-[22.4px] md:text-[1.50rem] lg:text-[1.50rem] text-purple"
           />
         </div>
